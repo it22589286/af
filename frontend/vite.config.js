a8047122833c6548e9594d73c8c5246f2ec24c1d
Nipun Lahiru <it22589286@my.sliit.lk>
2025-05-04 23:18:54 +0530

    afsdfd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+        format: 'es',
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
+  }
 })
